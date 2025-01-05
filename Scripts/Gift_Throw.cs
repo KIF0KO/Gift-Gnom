46fb72618d51d1a2b37f4919df34589be8807558
KIF0KO <iblowerB@gmail.com>
2025-01-05 23:37:33 +0300

    Unity мразь

--- Scripts/Gift_Throw.cs
+++ Scripts/Gift_Throw.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class Gift_Throw : MonoBehaviour
 {
     public GameObject gift;
+    public GameObject spawn_point;
+    private Rigidbody rb;
+    private Vector3 vector;
+
+    private void Start()
+    {
+        rb = gift.GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.E))
             Gift_Throwing();
+        vector = transform.position;
     }
 
     void Gift_Throwing()
     {
-        Instantiate(gift);
+        Instantiate(gift, spawn_point);
+        new Vector3();
+        rb.AddForce(Vector3.forward * 5);
     }
 }
